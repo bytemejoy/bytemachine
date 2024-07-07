38e519ef7f4d5b765c36c39f77968ba058f645b0
bytemejoy <bytemejoy@gmail.com>
2024-07-07 11:40:33 -0700

    Update main script for interactive control

--- src/main.py
+++ src/main.py
@@ -1,84 +1,135 @@
 import asyncio
-import time
+
+from pynput import keyboard
 
 from src.orca.actuator import OrcaActuator
-from src.orca import constants
+from src.orca.constants import OrcaConstants
+from src.bytemachine import lib_bytemachine
 
-ORCA_CONSTANTS = constants.OrcaConstants()
+ORCA_CONSTANTS = OrcaConstants()
+
+# Default values
+STROKE_LENGTH_MM = 0  # Starting stroke length
+STROKE_RATE_MM_S = 0  # Starting stroke rate
+
+# Limits
+MIN_STROKE_LENGTH_MM = 0
+MAX_STROKE_LENGTH_MM = 228
+MIN_STROKE_RATE_MM_S = 0
+MAX_STROKE_RATE_MM_S = 676
+
+# Increments
+STROKE_LENGTH_INCREMENT_MM = 25
+STROKE_RATE_INCREMENT_MM_S = 33
 
 
-async def main() -> None:
-    """A collection of sample interactions with an OrcaActuator."""
-    actuator = OrcaActuator(
-        port="/dev/tty.usbserial-FT8F0SP7"
-    )  # TODO(bytemejoy): Write auto port config logic
-
-    # Connect to the actuator.
-    conneciton_success = await actuator.connect()
-    print(f"Connected {conneciton_success}")
-
-    # Reset user specified configuration.
-    await actuator.reset_user_options()
-    await actuator.reset_kinematic_config()
-
-    # Get serial number.
-    serial_number = await actuator.get_serial_number()
-    if serial_number:
-        print(f"Serial number is {serial_number}")
-
-    # Get firmware version.
-    firmware_verison = await actuator.get_firmware_version()
-    if firmware_verison:
-        print(f"Serial number is {firmware_verison}")
-
-    # Auto zero out the actuator.
-    await actuator.set_mode(ORCA_CONSTANTS.MODE_SLEEP)
-    auto_zero_successful: bool = await actuator.command_auto_zero(
-        max_force_n=50, exit_to_mode=ORCA_CONSTANTS.MODE_SLEEP
+def print_usage():
+    """Prints current state and usage instructions"""
+    print(
+        f"Current stroke length {STROKE_LENGTH_MM}mm, stroke rate "
+        f"{STROKE_RATE_MM_S}mm/s\n"
+        "  Usage:\n"
+        "  - up-arrow: increase stroke rate\n"
+        "  - down-arrow: decrease stroke rate\n"
+        "  - left-arrow: decrease stroke length\n"
+        "  - right-arrow: increase stroke length\n"
+        "  - escape: exit"
     )
-    while True:
-        current_mode = await actuator.get_mode()
-        if current_mode and current_mode != ORCA_CONSTANTS.MODE_AUTO_ZERO:
-            break
-    print(f"Auto zero successful = {auto_zero_successful}")
 
-    # Limit the amount of force the shaft may exert.
-    await actuator.set_max_force(max_force_mn=44482)
 
-    # Configure kinematic motion.
-    kinematic_write_successful = await actuator.set_kinematic_motion(
-        motion_id=0,
-        position_target_um=200000,
-        settling_time_ms=500,
-        auto_start_delay_ms=0,
-        next_id=2,
-        motion_type=1,
-        auto_start_next=1,
+async def handle_state_change(
+    actuator: OrcaActuator, stroke_rate_mm_s, stroke_length_mm
+):
+    current_mode = await actuator.get_mode()
+    should_trigger = False
+    if current_mode:
+        if (
+            stroke_length_mm == 0 or stroke_rate_mm_s == 0
+        ) and current_mode != ORCA_CONSTANTS.MODE_SLEEP:
+            await actuator.set_mode(ORCA_CONSTANTS.MODE_SLEEP)
+        if (
+            stroke_length_mm > 0 or stroke_rate_mm_s > 0
+        ) and current_mode != ORCA_CONSTANTS.MODE_KINEMATIC:
+            await actuator.set_mode(ORCA_CONSTANTS.MODE_KINEMATIC)
+            should_trigger = True
+    await lib_bytemachine.configure_two_point_kinematic_motion(
+        actuator,
+        stroke_rate_mm_s=stroke_rate_mm_s,
+        stroke_length_mm=stroke_length_mm,
     )
-    kinematic_write_successful = (
-        await actuator.set_kinematic_motion(
-            motion_id=1,
-            position_target_um=50000,
-            settling_time_ms=500,
-            auto_start_delay_ms=0,
-            next_id=0,
-            motion_type=1,
-            auto_start_next=1,
-        )
-        and kinematic_write_successful
-    )
-    if kinematic_write_successful:
-        await actuator.set_mode(ORCA_CONSTANTS.MODE_KINEMATIC)
-        triggered = await actuator.trigger_kinematic_motion(motion_id=0)
-        print(f"Triggered motion success = {triggered}")
+    if should_trigger:
+        await actuator.trigger_kinematic_motion(motion_id=0)
 
-    # Let the kinematic motion run for ten seconds.
-    time.sleep(10)
 
-    # Set the mode to sleep.
-    await actuator.set_mode(ORCA_CONSTANTS.MODE_SLEEP)
+async def main():
+    """Main entry point for the Orca actuator control script."""
+    actuator = OrcaActuator(port="/dev/tty.usbserial-FT8F0SP7")
+    connected = await actuator.connect()
+    if not connected:
+        print("Failed to connect to the actuator. Exiting.")
+        return
 
-    # Close the Modbus connection
+    # Perform auto-zero before starting
+    print("Performing auto-zero...")
+    auto_zero_success = await lib_bytemachine.auto_zero_wait(actuator)
+    if not auto_zero_success:
+        print("Auto-zero failed. Exiting.")
+        return
+    print("Auto-zero complete.")
+
+    def on_press(key):
+        """Handles key presses for adjusting stroke length and speed."""
+        global STROKE_LENGTH_MM, STROKE_RATE_MM_S
+        changed: bool = False
+        try:
+            if key == keyboard.Key.left:
+                STROKE_LENGTH_MM = max(
+                    STROKE_LENGTH_MM - STROKE_LENGTH_INCREMENT_MM,
+                    MIN_STROKE_LENGTH_MM,
+                )
+                changed = True
+            elif key == keyboard.Key.right:
+                STROKE_LENGTH_MM = min(
+                    STROKE_LENGTH_MM + STROKE_LENGTH_INCREMENT_MM,
+                    MAX_STROKE_LENGTH_MM,
+                )
+                changed = True
+            elif key == keyboard.Key.up:
+                STROKE_RATE_MM_S = min(
+                    STROKE_RATE_MM_S + STROKE_RATE_INCREMENT_MM_S,
+                    MAX_STROKE_RATE_MM_S,
+                )
+                changed = True
+            elif key == keyboard.Key.down:
+                STROKE_RATE_MM_S = max(
+                    STROKE_RATE_MM_S - STROKE_RATE_INCREMENT_MM_S,
+                    MIN_STROKE_RATE_MM_S,
+                )
+                changed = True
+
+            if changed:
+                asyncio.create_task(
+                    handle_state_change(actuator, STROKE_RATE_MM_S, STROKE_LENGTH_MM)
+                )
+                print_usage()
+
+        except AttributeError:
+            print("Invalid key pressed: {0}".format(key))
+            print_usage()
+
+    def on_release(key):
+        """Handles key releases."""
+        if key == keyboard.Key.esc:
+            # Stop listener and exit the script
+            return
+
+    # Print current state and
+    print_usage()
+
+    # Collect events until esc is pressed.
+    with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
+        listener.join()
+
     actuator.close()
 
 
